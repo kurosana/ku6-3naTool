d32cb73722e3a2ac51dd6478ce590f8f28c273e4
kurosana <91244139+kurosana@users.noreply.github.com>
2026-03-21 12:14:42 +0900

    v1.3.1
    シャドウブリガロン・ギルガルド

--- config.js
+++ config.js
@@ -56,7 +56,7 @@ const CONFIG = {
   templatePath: "Image/Template.png",
 
   // ========== バージョン表記 ==========
-  appVersion: "v1.3.0",
+  appVersion: "v1.3.1",
 
   // ========== 英語出力フォントサイズ ==========
   outputEngPokemonNameSize: 57,
