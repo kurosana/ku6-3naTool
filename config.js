afd47a7a388ac04d7f9aba4fb97429c6dc42b8c8
kurosana <91244139+kurosana@users.noreply.github.com>
2026-09-13 01:01:26 +0900

    v3.0.5

--- config.js
+++ config.js
@@ -5,11 +5,17 @@
 const CONFIG = {
   debugRecognition: 0,
 
-  appVersion: "v3.0.4",
+  appVersion: "v3.0.5",
   appReleaseNotes: "UI全面改修・保存スロット10件・対戦ログ・メガ風デザイン",
 
   // ユーザー向け変更履歴（新しいバージョンは先頭に追記）
   appChangelog: [
+    {
+      version: "v3.0.5",
+      changes: [
+        "黄昏の旅路シーズンに合わせて技の優先度を変更しました。",
+      ],
+    },
     {
       version: "v3.0.4",
       changes: [
